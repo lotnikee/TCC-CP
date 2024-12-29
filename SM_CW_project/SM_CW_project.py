5b687511c6f649f2a227c5c3244547df613407fc
lot nikee <lotnikee@github.com>
2024-12-29 21:57:24 +0100

    commit

--- SM_CW_project/SM_CW_project.py
+++ SM_CW_project/SM_CW_project.py
@@ -3,7 +3,6 @@ from ase.build import add_adsorbate, fcc111, molecule
 from ase.calculators.emt import EMT
 from ase.constraints import FixAtoms
 from ase.optimize import QuasiNewton
-from ase.visualize import view
 from ase.thermochemistry import IdealGasThermo, HarmonicThermo
 
 # Get the energy of a CH4 molecule 
@@ -24,9 +23,27 @@ CH4_ads.set_constraint(constraint)
 dyn = QuasiNewton(CH4_ads, trajectory="CH4_Cu(111).traj")
 dyn.run(fmax=0.05)
 energy_CH4_ads = CH4_ads.get_potential_energy()
-view(CH4_ads)
 
 # A) Calculate adsoroption energy
 adsorption_energy_CH4 = energy_CH4_ads - (energy_slab + energy_CH4_gas)
 print(f"Adsorption energy of CH4 on Cu(111): {adsorption_energy_CH4: .3f} eV")
 
+# B) Calculate Gibb's Free energy of adsorption of CH4 on Cu(111) at 300K and 1 bar
+vib_energy_CH4_gas = [0.3843, 0.3840, 0.3840, 0.3685, 0.1881, 0.1879, 0.1595, 0.1593, 0.1592]
+vib_energy_CH4_ads = [0.3815, 0.3758, 0.3758, 0.3625, 0.1850, 0.1848, 0.1589, 0.1584, 0.1559, 0.0161, 0.0161, 0.0112, 0.0061, 0.0061, 0.0061]
+thermo_CH4_gas = IdealGasThermo(vib_energies=vib_energy_CH4_gas,
+                        geometry="nonlinear",
+                        potentialenergy=energy_CH4_gas,
+                        atoms=CH4_molec,
+                        symmetrynumber=12,
+                        spin=0)
+thermo_CH4_ads = HarmonicThermo(vib_energies=vib_energy_CH4_ads, potentialenergy=energy_CH4_ads)
+temp = 300
+pressure = 1.0e+5
+
+g_CH4_gas = thermo_CH4_gas.get_gibbs_energy(temperature=temp, pressure=pressure, verbose=False)
+g_CH4_ads = thermo_CH4_ads.get_helmholtz_energy(temperature=temp, verbose=False)
+g_slab = energy_slab
+Pa_to_bar = 1.0e-5
+adsorption_free_energy_CH4 = g_CH4_ads - (g_slab + g_CH4_gas)
+print(f"Adsorption free energy of CH4 on Cu(111) at {temp}K and {pressure*Pa_to_bar} bar: {adsorption_free_energy_CH4: .3f} eV")
